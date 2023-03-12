0e9d3970b28e7dfb885a7817540af62c8bd04d2a
Manuel <info@tagirijus.de>
2023-03-12 14:07:37 +0100

    Tagi Shortcuts added

---
+++ Assets/js/tagi-shortcuts.js
@@ -0,0 +1,44 @@
+function tagiShortcuts(original) {
+	return () => {
+		function goToLink (selector) {
+			if (! KB.modal.isOpen()) {
+				var element = KB.find(selector);
+
+				if (element !== null) {
+					window.location = element.attr('href');
+				}
+			}
+		}
+
+		original();
+
+		KB.onKey('v+a', function () {
+			window.location = '/bigboard';
+		});
+
+		KB.onKey('v+t', function () {
+			if (! KB.modal.isOpen()) {
+				var reg = /\/user\/show\/(\d+)/;
+				var html = document.documentElement.innerHTML;
+				var match = html.match(reg);
+				if (match) {
+					var userId = match[1];
+					window.location = '/dashboard/' + userId + '/tasks';
+				}
+			}
+		});
+
+		KB.onKey('a+a', function () {
+			if (! KB.modal.isOpen()) {
+				var projectId = document.getElementById('form-project_id');
+				if (projectId != null) {
+					var projectId = projectId.value;
+					KB.modal.open('/analytics/tasks/' + projectId, 'large', false);
+				}
+			}
+		});
+
+	};
+}
+
+KB.keyboardShortcuts = tagiShortcuts(KB.keyboardShortcuts);
