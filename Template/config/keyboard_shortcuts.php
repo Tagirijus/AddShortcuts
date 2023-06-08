9f2c7f09868f87f4771528450669143003b85cb2
Manuel <info@tagirijus.de>
2023-06-08 21:57:17 +0200

    Added missing help text

--- Template/config/keyboard_shortcuts.php
+++ Template/config/keyboard_shortcuts.php
@@ -26,6 +26,7 @@
         <li><?= t('Add spent time') ?> = <strong>t</strong><br><i>(need <a href="https://github.com/Tagirijus/AddSpentTime" target="_blank">AddSpentTime</a> for this to work)</i></li>
         <li><?= t('Convert task times to new subtask') ?> = <strong>S</strong><br><i>(need <a href="https://github.com/Tagirijus/SubtaskHelper" target="_blank">SubtaskHelper</a> for this to work)</i></li>
         <li><?= t('Combine done subtasks to new subtask') ?> = <strong>g</strong><br><i>(need <a href="https://github.com/Tagirijus/SubtaskHelper" target="_blank">SubtaskHelper</a> for this to work)</i></li>
+        <li><?= t('Edit subtask') ?> = <strong>E</strong><br><i>(need <a href="https://github.com/Tagirijus/SubtaskHelper" target="_blank">SubtaskHelper</a> for this to work)</i></li>
     </ul>
     <h3><?= t('Application') ?></h3>
     <ul>
